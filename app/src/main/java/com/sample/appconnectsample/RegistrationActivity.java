55c0ae8224bc5886530131785d59022c8baf9a5f
Adam Godel <agodel@mdsol.com>
2016-08-26 16:14:04 -0400

    address comments

--- app/src/main/java/com/sample/appconnectsample/RegistrationActivity.java
+++ app/src/main/java/com/sample/appconnectsample/RegistrationActivity.java
@@ -1,13 +1,7 @@
 package com.sample.appconnectsample;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.mdsol.babbage.net.RequestException;
 
 import java.util.HashMap;
@@ -20,6 +14,9 @@ public abstract class RegistrationActivity extends AppCompatActivity {
 
     public static final int REGISTRATION_REQUEST = 1;
 
+    public static final HashMap<RequestException.ErrorCause, String> errorMap = createErrorMap();
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REGISTRATION_REQUEST) {
@@ -28,23 +25,19 @@ public abstract class RegistrationActivity extends AppCompatActivity {
         }
     }
 
-    public HashMap<RequestException.ErrorCause, String> errorMap()
-
-    {
+    private static HashMap<RequestException.ErrorCause, String> createErrorMap() {
         HashMap<RequestException.ErrorCause, String> errorsMessages = new HashMap<RequestException.ErrorCause, String>();
         errorsMessages.put(RequestException.ErrorCause.EMAIL_ALREADY_EXISTS, "The user already exists in the study");
         errorsMessages.put(RequestException.ErrorCause.INVALID_PASSWORD, "Your password does not meet our requirements");
         return errorsMessages;
     }
 
-    public String getErrorMessageFromException(RequestException exception)
-
-    {
+    public String getErrorMessageFromException(RequestException exception) {
 
         String errorMessage = exception.getMessage();
-        String userMessage = errorMap().get(exception.getErrorCause());
+        String userMessage = errorMap.get(exception.getErrorCause());
 
-        if (!(userMessage == null))
+        if (userMessage != null)
         {
             errorMessage = userMessage;
         }
